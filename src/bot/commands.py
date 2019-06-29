98ef90024da5ccf4760712d42b8e28585c2c7faa
Valentin Diard <valentin.diard@epitech.eu>
2019-06-29 23:29:46 +0200

    [FIX] - Improve graphical style of the help message

--- src/bot/commands.py
+++ src/bot/commands.py
@@ -14,7 +14,7 @@ async def hello_cmd(message):
     await message.channel.send('I heard you! {0.name}'.format(message.author))
 
 async def help_cmd(message):
-    await message.channel.send('```Commands:\n- !help\n- !hello\n\nGame:\n- !tic_tac_toe\n\nOther:\n- !github```')
+    await message.channel.send('```md\n# Commands:\n- !help\n- !hello\n\n# Game:\n- !tic_tac_toe\n\n# Other:\n- !github```')
 
 async def github_cmd(message):
     await message.channel.send('To follow the project, report an issue or contributing => ' + GITHUB_LINK)
